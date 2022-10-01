Musafir02/device_xiaomi_sm6150-common

sm6150-common: Enable bluetooth profiles with product.prop
Bug: 217448211
Test: make -j
Change-Id: Idd7e52b7822915ed61299f1c0b29bf87ca0339fe

BoardConfigCommon.mk
@@ -125,6 +125,7 @@ TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 

sm6150-motorola/android_device_motorola_sm6150-common

BoardConfigCommon.mk
@@ -94,6 +94,7 @@ TARGET_MODULE_ALIASES += \
     wlan.ko:qca_cld3_wlan.ko
 
 # Platform
+TARGET_PRODUCT_PROP += $(VENDOR_PATH)/product.prop
 BOARD_USES_QCOM_HARDWARE := true
 QCOM_BOARD_PLATFORMS += sm6150
 TARGET_BOARD_PLATFORM := sm6150