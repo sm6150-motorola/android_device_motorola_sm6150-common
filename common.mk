ae045782fed2545d2c7421ecdc194e6f3f4e68a9
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    sm6150-common: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Iafb39fed27c23da884eb930616325e6fc7961f13

--- common.mk
+++ common.mk
@@ -493,7 +493,7 @@ PRODUCT_PRODUCT_VNDK_VERSION := current
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
