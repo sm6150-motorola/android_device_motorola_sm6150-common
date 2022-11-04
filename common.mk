d3651fa4593b19cbf05b3735f00013795b91f8f8
dianlujitao <dianlujitao@lineageos.org>
2022-11-04 15:21:34 +0800

    sm6150-common: Build moto common overlay
    Change-Id: I2547000945e816a5b6cc9876bef4a8430cf38754

--- common.mk
+++ common.mk
@@ -346,9 +346,10 @@ PRODUCT_PACKAGES += \
     libchrome \
     libchrome.vendor
 
-# MotoActions
+# Moto hardware
 PRODUCT_PACKAGES += \
-    MotoActions
+    MotoActions \
+    MotoCommonOverlay
 
 # Net
 PRODUCT_PACKAGES += \
